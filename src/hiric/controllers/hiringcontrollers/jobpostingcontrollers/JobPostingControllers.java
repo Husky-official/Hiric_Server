7a1979d9e1aac9f14698d79c512fd94846c8b5fb
Gashugi Aderline <aderlinecarmella@gmail.com>
2022-02-28 08:40:54 +0200

    updates

--- src/hiric/controllers/hiringcontrollers/jobpostingcontrollers/JobPostingControllers.java
+++ src/hiric/controllers/hiringcontrollers/jobpostingcontrollers/JobPostingControllers.java
@@ -1,7 +1,11 @@
 package hiric.controllers.hiringcontrollers.jobpostingcontrollers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
+/*
+ * author: Gashugi Aderline
+ * desc: This is a controller that handles actions including getting, creating, updating and deleting a job post.
+ *
+ */
 
 public class JobPostingControllers {
     public String mainMethod(JsonNode request) throws Exception {
