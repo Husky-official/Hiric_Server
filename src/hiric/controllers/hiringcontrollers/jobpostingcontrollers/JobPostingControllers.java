9f04148edbf66b4bef9de7af2adcb021b301b8e9
Gashugi Aderline <aderlinecarmella@gmail.com>
2022-02-28 08:16:01 +0200

    create a job post

--- src/hiric/controllers/hiringcontrollers/jobpostingcontrollers/JobPostingControllers.java
+++ src/hiric/controllers/hiringcontrollers/jobpostingcontrollers/JobPostingControllers.java
@@ -1,4 +1,22 @@
 package hiric.controllers.hiringcontrollers.jobpostingcontrollers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+
 public class JobPostingControllers {
+    public String mainMethod(JsonNode request) throws Exception {
+        String action = request.get("action").asText();
+
+        String response = "";
+
+        switch (action) {
+            case "createJobPost":
+                response = new JobPostingActions().createJobPost(request);
+                return response;
+            case "getJobPosts":
+                response = new JobPostingActions().getJobPosts(request);
+                return response;
+        }
+        return "";
+    }
 }
