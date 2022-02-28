477528752794158fb4e2a562eadce3dcbf5d2c23
henriette-bizy <hopebiziyaremye@gmail.com>
2022-02-28 07:31:34 +0200

    message/records controlling

---
+++ src/controllers/messagecontrollers/MessageController.java
@@ -0,0 +1,25 @@
+package controllers.usercontrollers;
+
+
+/**
+ * @author  Biziyaremye Henriette
+ * @description The controller for messages
+ */
+
+public class MessageController {
+
+    public MessageController(){}
+
+    public void RecordMessage(String Message){
+
+        
+
+
+
+    }
+
+
+
+
+
+}
