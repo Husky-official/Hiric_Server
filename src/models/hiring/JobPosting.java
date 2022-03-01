9d417eec2292639fb502c0526f330bf1586adeeb
Christian-pprogrammer <mpanoc6@gmail.com>
2022-03-01 07:47:57 +0200

    add job posting model

--- src/models/hiring/JobPosting.java
+++ src/models/hiring/JobPosting.java
@@ -1,2 +1,114 @@
-package models.hiring;public class JobPosting {
+package models.hiring;
+
+
+import java.util.Date;
+import java.sql.*;
+
+public class JobPosting {
+    private String jobId;
+    private String userId;
+    private String jobTitle;
+    private String jobDescription;
+    private String jobRequirements;
+    private String location;
+    private String startDate;
+    private String duration;
+    private int salary;
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String getJobRequirements() {
+        return jobRequirements;
+    }
+
+    public void setJobRequirements(String jobRequirements) {
+        this.jobRequirements = jobRequirements;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public JobPosting(String jobId, String userId, String jobTitle, String jobDescription, String jobRequirements, String location, String startDate, String duration, int salary) {
+        this.jobId = jobId;
+        this.userId = userId;
+        this.jobTitle = jobTitle;
+        this.jobDescription = jobDescription;
+        this.jobRequirements = jobRequirements;
+        this.location = location;
+        this.startDate = startDate;
+        this.duration = duration;
+        this.salary = salary;
+    }
+    @Override
+    public String toString() {
+        return "JobPosting{" +
+                "jobId=" + jobId +
+                "userId=" + userId +
+                ", jobTitle=" + jobTitle +
+                ", jobDescription='" + jobDescription + '\'' +
+                ", jobRequirements=" + jobRequirements +
+                ", location=" + location +
+                ", startDate=" + startDate +
+                ", duration=" + duration +
+                ", salary=" + salary +
+                '}';
+    }
 }
