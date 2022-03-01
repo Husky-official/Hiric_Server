7fd8164c7abacdf63ee8d2b432defb9a24ac1c43
Christian-pprogrammer <mpanoc6@gmail.com>
2022-03-01 07:42:43 +0200

    add Job controllers with the get user action

--- src/controllers/jobs/JobControllers.java
+++ src/controllers/jobs/JobControllers.java
@@ -1,2 +1,23 @@
-package controllers.jobs;public class JobControllers {
+package controllers.jobs;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import controllers.usercontrollers.UserActions;
+
+public class JobControllers {
+    public String mainMethod(JsonNode request) throws Exception{
+        String action = request.get("action").asText();
+
+        String response = "";
+
+        switch (action){
+            case "get_user_jobs":
+                response = new JobActions().getJobs(request);
+                System.out.println(response);
+                return  response;
+            default:
+                System.out.println("Unknown action");
+        }
+
+        return "";
+    }
 }
