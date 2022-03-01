14b951cbeb62828d54bfb0943ae9533f725f5a83
Christian-pprogrammer <mpanoc6@gmail.com>
2022-03-01 07:47:29 +0200

    add class to represent returned jobs in respone

--- src/controllers/jobs/ReturnJobs.java
+++ src/controllers/jobs/ReturnJobs.java
@@ -1,2 +1,12 @@
-package controllers.jobs;public class ReturnJobs {
+package controllers.jobs;
+
+import models.hiring.JobPosting;
+
+import java.util.ArrayList;
+
+public class ReturnJobs {
+    public ArrayList<JobPosting> jobPosts;
+    public ReturnJobs(ArrayList<JobPosting> jobPosts) {
+        this.jobPosts = jobPosts;
+    }
 }
