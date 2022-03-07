4e258f881637faf96bc04baecfb660f3cc193430
Anglebert-Dev <anglebertsh@gmail.com>
2022-03-07 09:44:21 +0200

    added logout

--- src/controllers/usercontrollers/UserControllers.java
+++ src/controllers/usercontrollers/UserControllers.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class UserControllers {
     public String mainMethod(JsonNode request) throws Exception{
         String action = request.get("action").asText();
-//        System.out.println(action);
+//      System.out.println(action);
         String response = "";
 
         switch (action){
             case "login":
                 response = new UserActions().login(request);
+                return  response;
+            case "logout":
+                response = new UserActions().logout(request);
                 return  response;
             default:
                 System.out.println("Unknown action");
