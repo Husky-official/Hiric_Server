1c4db25fae41ddd8fafced820b7881b1ac945624
Christian-pprogrammer <mpanoc6@gmail.com>
2022-03-11 08:06:33 +0200

    shortlisting controller added and finished

--- src/controllers/shortListing/ShortListingController.java
+++ src/controllers/shortListing/ShortListingController.java
@@ -1,2 +1,17 @@
-package controllers.shortListing;public class ShortListingController {
+package controllers.shortListing;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import controllers.jobApplication.JobApplicationActions;
+
+public class ShortListingController {
+    public static String mainMethod(JsonNode request) throws Exception {
+        String action = request.get("action").asText();
+        String response = "";
+        switch (action) {
+            case "add to shortlist":
+                response = new ShortListingActions().addToShortList(request);
+                return response;
+        }
+        return response;
+    }
 }
