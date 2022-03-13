ae36cd93f419f109aabc059567bfefc087ff26f6
dabagirevalens <vdabagire@gmail.com>
2022-03-13 18:11:10 +0200

    delete group

--- src/controllers/groupmessaging/GroupActions.java
+++ src/controllers/groupmessaging/GroupActions.java
@@ -7,6 +7,7 @@ import models.ResponseStatus;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -47,6 +48,70 @@ public class GroupActions {
             responseStatus.setStatus(400);
             responseStatus.setActionToDo("CREATE GROUP");
             responseStatus.setMessage("Unable to create new group");
+        }
+
+        return new ObjectMapper().writeValueAsString(responseStatus);
+    }
+
+    public String joinGroup(JsonNode requestBody) throws Exception{
+
+        ResponseStatus responseStatus = new ResponseStatus();
+
+        Connection connection = new OnlineDbConnection().getConnection();
+        JsonNode groupInfo = requestBody.get("object");
+
+        Iterator<Map.Entry<String, JsonNode>> iterator = groupInfo.fields();
+
+        int member_id = Integer.parseInt(iterator.next().toString().split("=")[1]);
+        int group_id = Integer.parseInt(iterator.next().toString().split("=")[1]);
+
+        String groupMemberCreationQuery = "INSERT INTO `Groupmembers` (`memberID`, `groupID`, `membershipStatus`, `membershipRole`) VALUES (?, ?, ?, ?)";
+        PreparedStatement st = connection.prepareStatement(groupMemberCreationQuery);
+        st.setInt(1,member_id);
+        st.setInt(2, group_id);
+        st.setString(3, "in");
+        st.setString(4, "member");
+
+        int count  = st.executeUpdate();
+
+        if(count>0){
+            responseStatus.setStatus(200);
+            responseStatus.setActionToDo("JOINING GROUP");
+            responseStatus.setMessage("New Member Has Joined Successfully");
+        }else{
+            responseStatus.setStatus(400);
+            responseStatus.setActionToDo("JOINING GROUP");
+            responseStatus.setMessage("Unable to JOIN  group");
+        }
+
+        return new ObjectMapper().writeValueAsString(responseStatus);
+    }
+
+    public String deleteGroup(JsonNode requestBody) throws Exception{
+
+        ResponseStatus responseStatus = new ResponseStatus();
+
+        Connection connection = new OnlineDbConnection().getConnection();
+        JsonNode groupInfo = requestBody.get("object");
+
+        Iterator<Map.Entry<String, JsonNode>> iterator = groupInfo.fields();
+
+        int group_id = Integer.parseInt(iterator.next().toString().split("=")[1]);
+
+        String deleteGroupQuery = "DELETE FROM `Groups` WHERE `Groups`.`id` = ?";
+        PreparedStatement st = connection.prepareStatement(deleteGroupQuery);
+        st.setInt(1, group_id);
+
+        int count  = st.executeUpdate();
+
+        if(count>0){
+            responseStatus.setStatus(200);
+            responseStatus.setActionToDo("DELETE GROUP");
+            responseStatus.setMessage("Group Has Deleted Successfully");
+        }else{
+            responseStatus.setStatus(400);
+            responseStatus.setActionToDo("DELETE GROUP");
+            responseStatus.setMessage("Unable to DELETE  group");
         }
 
         return new ObjectMapper().writeValueAsString(responseStatus);
